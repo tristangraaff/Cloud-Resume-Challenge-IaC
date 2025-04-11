d1e813d002918ae9a26a11842b96e8b75391def1
Tristan Graaff <tristangraaff@yahoo.com>
2025-04-11 18:48:09 +0200

    set up jest and babel for testing es6 modules

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  // Tell Jest to treat .mjs files as ES modules.
+  //extensionsToTreatAsEsm: ['.mjs'],
+
+  // Use babel-jest to transform the ES module code.
+  transform: {
+    '^.+\\.mjs$': 'babel-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.mjs$',
+  // Optionally specify the test environment (the default is 'node').
+  testEnvironment: 'node'
+};
